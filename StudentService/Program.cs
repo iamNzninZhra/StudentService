ca5afb0ca8278835bd02776d4bf639a4c76ef6f6
iamNzninZhra <iamnazaninzahra2002@gmail.com>
2024-07-26 22:52:19 +0330

    Initial commit

---
+++ StudentService/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stdService = new Student();
+            stdService.Name = "NazaninZahra";
+            stdService.LastName = "Shahsavan";
+            stdService.PhoneNumber = 09391381755;
+            stdService.BirthDay = 04;
+            stdService.BirthMonth = 12;
+            //stdService.BirthYear = Console.ReadLine("Please Enter your year of birth");
+
+
+            Console.WriteLine($"Full Name : {stdService.Name} {stdService.LastName}");
+            //Console.writeline("lastname :" + stdservice.lastname);
+            Console.WriteLine("Phonenumber :" + stdService.PhoneNumber);
+            Console.WriteLine($"Birthday : {stdService.BirthDay}/{stdService.BirthMonth}/{stdService.BirthYear}");
+
+            stdService.SchoolAge();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StudentService/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace StudentService
+{
+    public class Student
+    {
+        public string Name;
+        public string LastName;
+        public long PhoneNumber;
+        public int BirthDay;
+        public int BirthMonth;
+        public int BirthYear;
+
+        private int InputBirthYear()
+        {
+            Console.Beep();
+            var strYear = Console.ReadLine();
+            if (strYear != null)
+            {
+                Convert.ToInt32(strYear);
+                int resAge;
+                if (int.TryParse(strYear, out resAge))
+                {
+                    return resAge;
+                }
+            }
+            return 0;
+            //اینجا میخوام سن رو از یوزر بگیرم
+        }
+
+
+        public bool SchoolAge()
+        {
+            Console.WriteLine("Please enter your age?");
+            var birthYear = InputBirthYear();
+            if (birthYear > 0)
+            {
+                if (birthYear >= 2017)
+                {
+                    Console.WriteLine("Able to sign up for school");
+                    this.BirthYear = birthYear;
+                    return true;
+                }
+                else
+                {
+                    
+                    Console.WriteLine("Not able to sign up for school");
+                }
+            }
+            else
+            {
+                // print error
+            }
+            //بعد اینجا بگم اگه هفت سالشه میتونه بره مدرسه اگه نیست نمیتونه بره
+
+
+            return false;
+
+        }
+
+    }
+
+
+}
